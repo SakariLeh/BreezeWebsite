5b78634c9359f4ce11f1f8fb6063a5e4e7967a47
Shama <sobitov2003shamsiddin@gmail.com>
2023-04-09 22:21:36 +0500

    some updates made

---
+++ products/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from products.models import Product, ProductCategory, Basket
+from users.models import User
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Test Title',
+        'username': 'valeriy',
+    }
+
+    return render(request, 'products/index.html', context)
+
+
+def products(request, category_id=None, page_number=1):
+    # if category_id:
+    #     products = Product.objects.filter(category_id=category_id)
+    # else:
+    #     products = Product.objects.all()
+
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
+
+    context = {
+        'title': 'Store - Католог',
+        'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
+    }
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, pk):
+    hotels = Product.objects.get(pk=pk)
+    context = {
+        'hotels': hotels,
+    }
+    return render(request, 'products/product_detail.html', context)
+
+
+@login_required
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+@login_required
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
